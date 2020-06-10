9476ed8162a6b76fe608c24843b1e8f94e695480
Zihan <zxie19@gmail.com>
2020-06-10 12:01:19 -0700

    Remove unnecessary code

--- sample-apps/nodejs-apig/4-get.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-APIID=$(aws cloudformation describe-stack-resource --stack-name nodejs-apig --logical-resource-id api --query 'StackResourceDetail.PhysicalResourceId' --output text)
-REGION=$(aws configure get region)
-
-curl https://$APIID.execute-api.$REGION.amazonaws.com/api/ -v
